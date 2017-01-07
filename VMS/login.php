97c7b8e3fa414ed523531d3bb6fb982a3fd1a490
hrishi-thombare <MyGithub@16>
2017-01-08 02:53:19 +0530

    login.php update

--- VMS/login.php
+++ VMS/login.php
@@ -1,10 +1,17 @@
 <?php
 	require 'config.php';
-	
-	if(isset($_POST) && $_POST['username'] && isset($_POST['password'])){
+	if(isset($_POST['username']) && isset($_POST['password'])){
 		$username = trim($_POST['username']);
+		
 		$password = trim($_POST['password']);
+		$query = "SELECT * FROM user_data WHERE username = '" . $username . "' AND password = '" . $password . "'";
+					
+		$result = mysqli_query($link, $query);
 		
-		
+		if(mysqli_num_rows($result) > 0) {
+			echo "Login successful !!";
+		} else {
+			echo "Login failed";
+		}
 	}
 ?>
